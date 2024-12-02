7288f335e953043fc3f64dbb34c03a04bb944010
Azzer8 <47861195+Azzer8@users.noreply.github.com>
2024-12-02 11:03:15 +0300

    Реализован operator() (доступ по индексу) класса MatrixDiag

--- Lab2/MatrixDiag.hpp
+++ Lab2/MatrixDiag.hpp
@@ -1,5 +1,4 @@
 #include <vector>
-#include <unordered_map>
 #include "MatrixDense.hpp"
 
 using namespace std;
@@ -7,83 +6,104 @@ using namespace std;
 
 template<typename T = double>
 class MatrixDiag {
-    vector<T> _diagonal_idx;
+    vector<int> _diagonals_idxs;
     vector<T> _data;
     size_t _row, _col;
  public:
     MatrixDiag(const MatrixDense<T>& matrix) {
         _row = matrix.get_row_col().first;
         _col = matrix.get_row_col().second;
+        
         if (_row != _col) {
             throw runtime_error("Ошибка! Матрица должна быть квадратной.");
         }
 
         // Обработка главной диагонали
-        bool hasNonZeroMain = false;
+        bool nonZeroDiag = false;
         for (size_t i = 0; i < _row; ++i) {
             T value = matrix(i, i);
             _data.push_back(value);
-            if (value != 0) {
-                hasNonZeroMain = true;
-            }
+            if (value != 0) { nonZeroDiag = true; }
         }
-        if (hasNonZeroMain) {
-            _diagonal_idx.push_back(0); // Главная диагональ
-        }
-        else {
-            _data.clear();
-        }
+
+        if (nonZeroDiag) { _diagonals_idxs.push_back(0); }
+        else { _data.clear(); }
 
         // Обработка диагоналей ниже главной
         for (size_t d = 1; d < _row; ++d) {
-            bool hasNonZeroLower = false;
+            nonZeroDiag = false;
             vector<T> lowerDiagonal;
             for (size_t i = 0; i < _row - d; ++i) {
                 T value = matrix(i + d, i);
                 lowerDiagonal.push_back(value);
-                if (value != 0) {
-                    hasNonZeroLower = true;
-                }
+                if (value != 0) { nonZeroDiag = true; }
             }
-            if (hasNonZeroLower) {
+            
+            if (nonZeroDiag) {
                 _data.insert(_data.end(), lowerDiagonal.begin(), lowerDiagonal.end());
-                _diagonal_idx.push_back(-static_cast<int>(d)); // Индекс для нижней диагонали
+                _diagonals_idxs.push_back(-int(d));
             }
         }
 
         // Обработка диагоналей выше главной
         for (size_t d = 1; d < _row; ++d) {
-            bool hasNonZeroUpper = false;
+            nonZeroDiag = false;
             vector<T> upperDiagonal;
             for (size_t i = 0; i < _row - d; ++i) {
                 T value = matrix(i, i + d);
                 upperDiagonal.push_back(value);
-                if (value != 0) {
-                    hasNonZeroUpper = true;
-                }
+                if (value != 0) { nonZeroDiag = true; }
             }
-            if (hasNonZeroUpper) {
+
+            if (nonZeroDiag) {
                 _data.insert(_data.end(), upperDiagonal.begin(), upperDiagonal.end());
-                _diagonal_idx.push_back(d); // Индекс для верхней диагонали
+                _diagonals_idxs.push_back(d);
             }
         }
     }
 
     T operator()(size_t row, size_t col) const {
         if (row >= _row || col >= _col) {
-            throw std::out_of_range("Индекс вне диапазона матрицы");
+            throw out_of_range("Индекс вне диапазона матрицы");
         }
 
+        int reqstd_diag = int(col - row);
+        size_t idx_of_rd;
+        bool found = false;
+
+        for (size_t i = 0; i < _diagonals_idxs.size(); ++i) {
+            if (_diagonals_idxs[i] == reqstd_diag) {
+                found = true;
+                idx_of_rd = i;
+                break;
+            }
+        }
+
+        if (found) {
+            size_t data_index = 0;
+            if (reqstd_diag != 0) {
+                for (size_t i = 0; i < idx_of_rd; ++i) {
+                        // Добавляем количество элементов на каждой предыдущей диагонали
+                        data_index += _row - abs(_diagonals_idxs[i]);
+                    }
+            }
+
+            if (reqstd_diag <= 0) { data_index += col; }
+            else { data_index += row; }
+
+            return _data[data_index];
+        }
+        
         return T(0);
     }
 
     void print() const {
         cout << "Массив индексов ненулевых диагоналей: ";
-        for (const auto& value : _diagonal_idx) {
+        for (const auto& value : _diagonals_idxs) {
             cout << value << " ";
         }
         cout << endl;
-        cout << "Массив ненулевых диагоналей: ";
+        cout << "MatrixDense: ";
         for (const auto& value : _data) {
             cout << value << " ";
         }
