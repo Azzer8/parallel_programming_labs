01fb2c2df7cb069a083e7d70b7a549695cc3b613
Azzer8 <47861195+Azzer8@users.noreply.github.com>
2024-12-19 14:36:57 +0300

    Класс Vector со всеми методами (последовательными)

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -16,10 +16,31 @@
 
 using namespace std;
 
+
+template<typename T>
+struct StructResult {
+    T _result;
+    double _time;
+
+    StructResult(T result, double time) : 
+        _result(result), _time(time) {}
+
+    void print() const {
+        cout << "[result: " << _result
+                << "; time: " << _time << "]" << endl;
+    }
+    
+    static bool compare(const StructResult<T>& left, const StructResult<T>& right) 
+    { 
+        return left._time < right._time; 
+    }
+};
+
+
 template<typename T = double>
 class Vector {
-    vector<T> _data;
     size_t _size;
+    vector<T> _data;
     bool _isInit = false;
 
 public:
@@ -43,74 +64,111 @@ public:
         _isInit = true;
     }
 
-    T sum() const {
+    T sum(size_t startIdx, size_t endIdx) const {
         if (_isInit) {
-            T result = 0;
-            for (const auto& val : _data)
-            {
-                result += val;
-            }
-            
-            return result;
+            if (endIdx < _size && endIdx > startIdx) {
+                T result = 0;
+                for (size_t i = startIdx; i < endIdx; ++i)
+                {
+                    result += _data[i];
+                }
+
+                return result;
+            } else {throw logic_error("Выход за пределы массива!");}
         }
         else {
             throw logic_error("Вектор не инициализирован!");
         }
     }
+    T sum(size_t endIdx) const { return sum(0, endIdx); }
+    T sum() const { return sum(0, _size - 1); }
 
-    T average() const {
-        if (!_isInit) throw logic_error("Вектор не инициализирован!");
-        T sum = 0;
-        for (const auto& val : _data) {
-            sum += val;
-        }
-
-        return sum / _size;
-    }
-
-    T euclideanNorm() const {
-        if (!_isInit) throw logic_error("Вектор не инициализирован!");
-        T sum = 0;
-        for (const auto& val : _data) {
-            sum += val * val;
-        }
-
-        return sqrt(sum);
-    }
-
-    T manhattanNorm() const {
-        if (!_isInit) throw logic_error("Вектор не инициализирован!");
-        T sum = 0;
-        for (const auto& val : _data) {
-            sum += abs(val);
-        }
-
-        return sum;
-    }
-
-    void print() const {
+    T mean(size_t startIdx, size_t endIdx) const {
         if (_isInit) {
-            for (size_t i = 0; i < _size; ++i)
-            {
-                cout << _data[i] << " ";
-            }
-            cout << endl;
-        } else {
+            if (endIdx < _size && endIdx > startIdx) {
+                T sum = 0;
+                for (size_t i = startIdx; i < endIdx; ++i)
+                {
+                    sum += _data[i];
+                }
+
+                return sum / endIdx - startIdx;
+            } else {throw logic_error("Выход за пределы массива!");}
+        }
+        else {
             throw logic_error("Вектор не инициализирован!");
         }
+    }
+    T mean(size_t endIdx) const { return mean(0, endIdx); }
+    T mean() const { return mean(0, _size - 1); }
+
+    T euclideanNorm(size_t startIdx, size_t endIdx) const {
+        if (_isInit) {
+            if (endIdx < _size && endIdx > startIdx) {
+                T sum = 0;
+                for (size_t i = startIdx; i < endIdx; ++i)
+                {
+                    sum += _data[i] * _data[i];
+                }
+
+                return sqrt(sum);
+            } else {throw logic_error("Выход за пределы массива!");}
+        }
+        else {
+            throw logic_error("Вектор не инициализирован!");
+        }
+    }
+    T euclideanNorm(size_t endIdx) const { return euclideanNorm(0, endIdx); }
+    T euclideanNorm() const { return euclideanNorm(0, _size - 1); }
+
+    T manhattanNorm(size_t startIdx, size_t endIdx) const {
+        if (_isInit) {
+            if (endIdx < _size && endIdx > startIdx) {
+                T result = 0;
+                for (size_t i = startIdx; i < endIdx; ++i)
+                {
+                    result += abs(_data[i]);
+                }
+
+                return result;
+            } else {throw logic_error("Выход за пределы массива!");}
+        }
+        else {
+            throw logic_error("Вектор не инициализирован!");
+        }
+    }
+    T manhattanNorm(size_t endIdx) const { return manhattanNorm(0, endIdx); }
+    T manhattanNorm() const { return manhattanNorm(0, _size - 1); }
+
+    Vector operator*(const Vector& v) const {
+        if (_isInit && v._isInit) {
+            if (_size == v._size) {
+                Vector result(_size);
+                for (size_t i = 0; i < _size; ++i) {
+                    result._data.push_back(_data[i] * v._data[i]);
+                }
+
+                return result;
+            }
+            else { throw logic_error("Векторы разного размера!");}
+        } else { throw logic_error("Векторы не инициализированы!"); }
     }
 
     void print(size_t startIdx, size_t endIdx) const {
         if (_isInit) {
-            for (size_t i = startIdx; i <= endIdx; ++i)
-            {
-                cout << _data[i] << " ";
-            }
-            cout << endl;
+            if (endIdx < _size && endIdx > startIdx) {
+                for (size_t i = startIdx; i <= endIdx; ++i)
+                {
+                    cout << fixed << setprecision(5) << _data[i] << " ";
+                }
+                cout << endl << endl;
+            } else {throw logic_error("Выход за пределы массива!");}
         } else {
             throw logic_error("Вектор не инициализирован!");
         }
     }
+    void print(size_t endIdx) const { return print(0, endIdx); }
+    void print() const { return print(0, _size - 1); }
 
 };
 
@@ -120,6 +178,7 @@ int main() {
     srand(time(0));
     size_t size = 10'000'000;
     Vector vec1(size);
-    vec1.fillRandom(-10.8, 10.8);
-    vec1.print(0, 10);
+    vec1.fillRandom(-10.5, 10.5);
+    vec1.print(10);
+    // cout << vec1.mean(10) << endl;
 }
